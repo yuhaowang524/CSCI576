c0fe1aaf567bcda76cab40d4ad638ebf95531f27
Yuhao Wang <yuhaowang524@icloud.com>
2022-10-28 11:31:03 -0700

    HW3 Programming part draft

---
+++ Assignment 3/DWT Compression/src/ImgDisplay.java
@@ -0,0 +1,28 @@
+// CSCI576 HW3
+// Name: Yuhao Wang
+// Student ID: 5779881695
+
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ImgDisplay {
+    // input file pixel height and width are set to 512
+    private static int height = 512;
+    private static int width = 512;
+
+    public void showImage(String[] args) {
+
+    }
+
+    private void displayPanel() {
+        JFrame jFrame = new JFrame();
+        GridBagLayout gbg = new GridBagLayout();
+    }
+
+
+    public static void main(String[] args) {
+        ImgDisplay ren = new ImgDisplay();
+        ren.showImage(args);
+    }
+}
